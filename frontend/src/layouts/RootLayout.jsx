0aee1164ff39108770c5847ac05697cbc6580d17
krishsitikhu <krishsitikhu28@gmail.com>
2024-09-02 15:52:26 +0545

    router set up

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,10 +1,17 @@
 
 import react from "react";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return <>
     This is root layouts
+    <ul>
+        <li><NavLink className="link" to="/">Home</NavLink> </li>
+        <li><NavLink className="link" to="/activity">Activity</NavLink> </li>
+        <li><NavLink className="link" to="/budgetplanner">Budget planner</NavLink> </li>
+        <li><NavLink  className="link" to="/priceguide">priceguide</NavLink> </li>
+        <li><NavLink className="link" to="/Help">Help and support</NavLink> </li>
+      </ul>
     <Outlet />
   </>
 };
